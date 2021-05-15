1b28a8b998a19b2193a3dd16c116bd621a0b2eb1
Rajkumar Das <rajkumardas2701@gmail.com>
2021-05-15 06:37:25 +0530

    Add UseHistory in SignUp Page

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -3,16 +3,18 @@ import '../styles/Signup.css';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import validator from 'email-validator';
-// import { useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { CircleToBlockLoading } from 'react-loadingg';
 import { authInit, authSuccess, authSignupFailure } from '../actions/index';
 import authCall from '../utils/apiCalls';
 import { userTypes } from '../constants/initialState';
+import NavBar from '../layouts/Navbar';
+import Footer from '../layouts/Footer';
 
 const Signup = ({
   signupinit, signupsuccess, signupfailure, isLoading, errors,
 }) => {
-  // const history = useHistory();
+  const history = useHistory();
   const [state, setState] = useState({
     firstname: '',
     lastname: '',
@@ -35,7 +37,7 @@ const Signup = ({
       password_confirmation: '',
       user_type: ['Recruiter', 'Candidate'],
     });
-    // history.push('/');
+    history.push('/');
   };
 
   const handleErrors = (errors) => {
@@ -75,77 +77,81 @@ const Signup = ({
   // }
 
   return (
-    <div className="signup-container">
-      <h2>Register to JobHub</h2>
-      <form onSubmit={handleSubmit} className="signup-form-container">
-        <div className="input-section">
-          <input
-            className="form-inputs"
-            placeholder="Firstname"
-            type="text"
-            name="firstname"
-            value={state.firstname}
-            onChange={handleChange}
-            required
-          />
-          <input
-            className="form-inputs"
-            placeholder="Lastname"
-            type="text"
-            name="lastname"
-            value={state.lastname}
-            onChange={handleChange}
-            required
-          />
-          <input
-            className="form-inputs"
-            placeholder="Email"
-            type="text"
-            name="email"
-            value={state.email}
-            onChange={handleChange}
-            required
-          />
-          <input
-            className="form-inputs"
-            placeholder="Password"
-            type="password"
-            name="password"
-            value={state.password}
-            onChange={handleChange}
-            required
-          />
-          <input
-            className="form-inputs"
-            placeholder="Confirm password"
-            type="password"
-            name="password_confirmation"
-            value={state.password_confirmation}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="signup-dropdown">
-          <p className="user-type-text">Signup As:</p>
-          <select className="user-type-dropdown" name="user_type" onChange={handleChange}>
-            {
+    <>
+      <NavBar />
+      <div className="signup-container">
+        <h2>Register to JobHub</h2>
+        <form onSubmit={handleSubmit} className="signup-form-container">
+          <div className="input-section">
+            <input
+              className="form-inputs"
+              placeholder="Firstname"
+              type="text"
+              name="firstname"
+              value={state.firstname}
+              onChange={handleChange}
+              required
+            />
+            <input
+              className="form-inputs"
+              placeholder="Lastname"
+              type="text"
+              name="lastname"
+              value={state.lastname}
+              onChange={handleChange}
+              required
+            />
+            <input
+              className="form-inputs"
+              placeholder="Email"
+              type="text"
+              name="email"
+              value={state.email}
+              onChange={handleChange}
+              required
+            />
+            <input
+              className="form-inputs"
+              placeholder="Password"
+              type="password"
+              name="password"
+              value={state.password}
+              onChange={handleChange}
+              required
+            />
+            <input
+              className="form-inputs"
+              placeholder="Confirm password"
+              type="password"
+              name="password_confirmation"
+              value={state.password_confirmation}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="signup-dropdown">
+            <p className="user-type-text">Signup As:</p>
+            <select className="user-type-dropdown" name="user_type" onChange={handleChange}>
+              {
               userTypes.map((ut) => <option className="user-type-option" key={ut} value={ut}>{ut}</option>)
             }
-          </select>
-        </div>
-        <div className="sign-up-button-section">
-          <button type="submit" className="signup-buttons">
-            Sign Up
-          </button>
-          <button type="button" className="signup-buttons" onClick={resetState}>
-            Cancel
-          </button>
-        </div>
-      </form>
-      {isLoading && <div><CircleToBlockLoading size="small" color="rgb(92, 92, 241)" /></div>}
-      <div className="form-validation" id="form-validation" />
-      <div className="server-error-section">{errors.length ? handleErrors(errors) : null}</div>
-    </div>
+            </select>
+          </div>
+          <div className="sign-up-button-section">
+            <button type="submit" className="signup-buttons">
+              Sign Up
+            </button>
+            <button type="button" className="signup-buttons" onClick={resetState}>
+              Cancel
+            </button>
+          </div>
+        </form>
+        {isLoading && <div><CircleToBlockLoading size="small" color="rgb(92, 92, 241)" /></div>}
+        <div className="form-validation" id="form-validation" />
+        <div className="server-error-section">{errors.length ? handleErrors(errors) : null}</div>
+      </div>
+      <Footer />
+    </>
   );
 };
 
