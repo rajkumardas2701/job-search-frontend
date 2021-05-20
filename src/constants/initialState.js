46a84d8839eeac936a09a6f226f350bfe7244c00
Rajkumar Das <rajkumardas2701@gmail.com>
2021-05-20 08:18:04 +0530

    Add postJobreducer

--- src/constants/initialState.js
+++ src/constants/initialState.js
@@ -14,6 +14,7 @@ const initialState = {
   postJobData: {
     isLoading: false,
     errors: [],
+    job: [],
   },
 };
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers } from 'redux';
 import { initialState } from '../constants/initialState';
 import authReducer from './authReducer';
 import jobsReducer from './jobsReducer';
+import postJobsReducer from './postJobsReducer';
 
 const rootReducer = combineReducers({
   auth: authReducer,
   jobsData: jobsReducer,
+  postJobData: postJobsReducer,
 });
 
 const store = createStore(rootReducer, initialState);
